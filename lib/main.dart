a6c5229d028795ad6c85d04f8c86d8c13fcb9a00
Alexandre Smirnov <sashasmart@gmail.com>
2017-11-27 22:30:57 +0100

    Start work on sqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,81 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 import 'single_workout.dart';
 import 'workout_list.dart';
 
+class Workout {
+
+  int id;
+  String type;
+  String date;
+
+  Workout({this.id, this.type, this.date});
+
+  Map toMap() {
+    Map map = new Map();
+    map['id'] = id;
+    map['type'] = type;
+    map['date'] = date;
+
+    return map;
+  }
+
+  Workout.fromMap(Map map) {
+    id = map[id];
+    type = map[type];
+    date = map[date];
+  }
+
+}
+
+connectToDatabase() async {
+  String dir = (await getApplicationDocumentsDirectory()).path;
+
+  print(dir + "assets/data.db");
+
+  Database database = await openDatabase(dir + "assets/data.db", version: 1,
+    onCreate: (Database db, int version) async {
+      print('successfully created');
+      // When creating the db, create the table
+      await db.execute(
+        "CREATE TABLE workouts (id INTEGER PRIMARY KEY, type TEXT, date TEXT)");
+    }
+  );
+
+  Workout test = new Workout(id: 3, type: "pull", date: "2017-11-13");
+
+  Map testMap = new Map();
+
+  testMap['id'] = 2;
+  testMap['type'] = "pull";
+  testMap['date'] = "2017-11-12";
+
+  //await database.insert("workouts", test.toMap());
+
+
+  /*
+  // Insert some records in a transaction
+  await database.inTransaction(() async {
+    int id1 = await database.rawInsert(
+        'INSERT INTO workouts(id, type, date) VALUES(2, "pull", "2017-11-11")');
+    print("inserted1: $id1");
+  });
+  */
+
+  List<Map> list = await database.rawQuery('SELECT * FROM workouts');
+  print(list[2]);
+
+}
+
 void main() {
+
+  connectToDatabase();
+
   runApp(new WorkoutTrackerClient());
 }
 
